a54419a8d969d4b8b31225634acf3b05b551e4cf
Alicia Gordo Azabal <43533576+aliciagaz@users.noreply.github.com>
2018-11-19 10:39:47 +0100

    Primera parte

--- client.py
+++ client.py
@@ -1,21 +1,23 @@
-#!/usr/bin/python3
+6#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+try:
+    SERVER = 'localhost'
+    METHOD = sys.argv[1]
+    RECEIVER = sys.argv[2][:sys.argv[2].find("@")]
+    IP = sys.argv[2][sys.argv[2].find("@")+1: sys.argv[2].rfind(":")]
+    PORT = int(sys.argv[2][sys.argv[2].rfind(":")+1:])
+    LINE = '¡Hola mundo!'
+    
+except IndexError:
+    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
