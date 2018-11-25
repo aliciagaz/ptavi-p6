c73097fa4bfee71d33213a3181ed9766e30c854d
Alicia Gordo Azabal <43533576+aliciagaz@users.noreply.github.com>
2018-11-25 23:48:26 +0100

    Servidor terminado con PEP8 y PEP257

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ try:
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
     AUDIO_FILE = sys.argv[3]
-    
+
 except IndexError:
     sys.exit("Usage: python3 server.py IP port audio_file")
 
@@ -21,12 +21,14 @@ RINGING = b"SIP/2.0 180 Ringing\r\n\r\n"
 OK = b"SIP/2.0 200 OK\r\n\r\n"
 BAD_REQUEST = b"SIP/2.0 400 Bad Request\r\n\r\n"
 METHOD_NOT_ALLOWED = b"SIP/2.0 405 Method Not Allowed\r\n\r\n"
-    
+
+
 class EchoHandler(socketserver.DatagramRequestHandler):
 
-    
     def handle(self):
+        # Escribe dirección y puerto del cliente (de tupla client-address).
         while 1:
+            # Lee línea a línea lo que nos envía el cliente.
             line = self.rfile.read()
             lista = ["INVITE", "ACK", "BYE"]
             lista_line_decode = line.decode("utf-8").split(" ")
@@ -49,8 +51,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             elif self.error(lista_line_decode):
                 self.wfile.write(BAD_REQUEST)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    # Creamos un servidor de eco y escuchamos.
     serv = socketserver.UDPServer(('', 6001), EchoHandler)
     print("Listening...")
     serv.serve_forever()
